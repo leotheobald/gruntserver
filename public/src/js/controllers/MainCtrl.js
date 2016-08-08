590b0cab36fe1acc41e7d287678d56b2b535a95b
Andy McIntyre <ndmaque@gmail.com>
2016-08-08 01:51:40 +0100

    added some dummy data to display

--- public/src/js/controllers/MainCtrl.js
+++ public/src/js/controllers/MainCtrl.js
@@ -7,11 +7,24 @@ angular.module('MainCtrl', [])
   $scope.message = 'A scope messsage!';
   $scope.data = [
     {
+        document: "Passport",
+        documentID: "PA DFTG-234",
         name: "Bob",
+        mode: "fa-user",
         arrival: "25 jan 2016"
     },
     {
+        document: "Passport",
+        documentID: "PA DFTG-2345",
         name: "Fred",
+        mode: "fa-car",
+        arrival: "25 Sep 2016"
+    },
+    {
+        document: "EU ID",
+        documentID: "EU-DFTG-2345",
+        name: "Caro",
+        mode: "fa-plane",
         arrival: "25 Sep 2016"
     }  
   ];
