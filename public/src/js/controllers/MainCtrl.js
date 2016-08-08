d96486beeb2b8532781f673fbafe56885d100778
Andy McIntyre <ndmaque@gmail.com>
2016-08-08 02:19:28 +0100

    added more dummy data to scope and stuff

--- public/src/js/controllers/MainCtrl.js
+++ public/src/js/controllers/MainCtrl.js
@@ -10,21 +10,21 @@ angular.module('MainCtrl', [])
         document: "Passport",
         documentID: "PA DFTG-234",
         name: "Bob",
-        mode: "fa-user",
+        mode: "user",
         arrival: "25 jan 2016"
     },
     {
         document: "Passport",
         documentID: "PA DFTG-2345",
         name: "Fred",
-        mode: "fa-car",
+        mode: "car",
         arrival: "25 Sep 2016"
     },
     {
         document: "EU ID",
         documentID: "EU-DFTG-2345",
-        name: "Caro",
-        mode: "fa-plane",
+        name: "Bonenfant",
+        mode: "plane",
         arrival: "25 Sep 2016"
     }  
   ];
